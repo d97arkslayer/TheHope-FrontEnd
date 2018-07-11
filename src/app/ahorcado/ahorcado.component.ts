5b4da0a9a491264d5dd53834d1337e89981fcdc8
Andrés Fernando Restrepo Sánchez <andres.1701411587@ucaldas.edu.co>
2018-07-10 19:34:53 -0500

    Ya se mandan las letras encontradas al FrontEnd
    El usuario manda sus letras y ya se verifican si prtenecen a la respuesta

--- src/app/ahorcado/ahorcado.component.ts
+++ src/app/ahorcado/ahorcado.component.ts
@@ -29,9 +29,34 @@ export class AhorcadoComponent implements OnInit {
  llenarPreguntas(){
 
     let p1 : Prengunta= new Prengunta();
-    p1.pregunta = " Análisis";
-    p1.respuesta = "suicidio";   
+    let p2 : Prengunta= new Prengunta();
+    let p3 : Prengunta= new Prengunta();
+    let p4 : Prengunta= new Prengunta();
+    let p5 : Prengunta= new Prengunta();
+    let p6 : Prengunta= new Prengunta();
+
+    p1.pregunta = " Mayor color de la bandera";
+    p2.pregunta = " Mejor equipo de futbol";
+    p3.pregunta = " Primer dia de la semana";
+    p4.pregunta = " Opuesto al blanco";
+    p5.pregunta = " Colegio del proyecto";
+    p6.pregunta = " Apellido de Esteban";
+
+
+    p1.respuesta = "amarillo";
+    p2.respuesta = "Real";
+    p3.respuesta = "lunes";
+    p4.respuesta = "negro";
+    p5.respuesta = "esperanza";
+    p6.respuesta = "herrera";  
+    
+    
     this.preguntas.push(p1);
+    this.preguntas.push(p2);
+    this.preguntas.push(p3);
+    this.preguntas.push(p4);
+    this.preguntas.push(p5);
+    this.preguntas.push(p6);
  }
 
 
@@ -40,14 +65,12 @@ export class AhorcadoComponent implements OnInit {
   this.respuestaTemporal=this.preguntas[indice].respuesta;
   console.log(this.preguntas[indice].respuesta.length);
  
-   // let respuesta : any[];
- // for (var i=0;i<this.preguntas[indice].respuesta.length;i++){
-  //  respuesta.push(this.preguntas[indice].respuesta.charAt(i));
+ 
   var respuesta:string[]= new Array(this.preguntas[indice].respuesta.length);
   for (var i=0;i<this.preguntas[indice].respuesta.length;i++){
     respuesta[i]=(this.preguntas[indice].respuesta.charAt(i));
   }
- // }
+
   this.respuestaActual= respuesta;
  //this.verificarLetra("i",respuesta);
 
@@ -55,17 +78,26 @@ export class AhorcadoComponent implements OnInit {
 
  verificarLetra(letra : string)
  {
+   var contador:number=0;
+   var bandera:boolean=true;
+
     this.respuestaActual.forEach(element => {
+      contador=contador+1;
       if (element===letra) {
-        console.log("Correcto "+ element);
-      }
+        alert("Letra correcta"+ contador);        
+        bandera=false;
+        (<HTMLInputElement>document.getElementById("labelResultados")).textContent=(<HTMLInputElement>document.getElementById("labelResultados")).textContent+letra;
+      }         
       
-    },
-    alert("Has ingresado una letra incorrecta lo sentimos") );
-    this.intentos= this.intentos-1;
-    if (this.intentos>0) {
-      alert("Intentalo otra vez, tienes "+ this.intentos)
+    });
+    if (contador>=this.respuestaActual.length && bandera) {
+      alert("Has ingresado una letra incorrecta lo sentimos"); 
+  this.intentos= this.intentos-1;
+  if (this.intentos>0) {
+    alert("Intentalo otra vez, tienes "+ this.intentos)
+  }
     }
+    
  
  }
 
